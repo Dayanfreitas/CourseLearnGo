6e787ab369b919b7ac552147edeb3ab3a55698b8
Dayan Freitas <dayan.freitas@catolicasc.org.br>
2021-10-23 17:02:54 -0300

    documentação

---
+++ Documentação/doc.go
@@ -0,0 +1,5 @@
+/*
+	Package de exemplo para aula de documentação
+	apenas escrita para evemplo
+*/
+package main
---
+++ Documentação/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+// X é uma constante
+const x = 10
+
+// doc função printa 1
+func doc() {
+	fmt.Println("Essa função eh um exemplo de documentação em go")
+}
+
+// doc função printa 2
+func doc2() {
+	fmt.Println("Essa função eh um exemplo de documentação em go2")
+}
+
+// Doc função printa 3
+func Doc3() {
+	fmt.Println("Essa função eh um exemplo de documentação em go 3")
+}

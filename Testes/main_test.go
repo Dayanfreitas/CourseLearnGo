36d7d54cf38c31fd9481e07762210fd29f4c13e9
Dayan Freitas <dayan.freitas@catolicasc.org.br>
2021-10-23 17:33:52 -0300

    teste em tabela

--- Testes/main_test.go
+++ Testes/main_test.go
@@ -2,6 +2,26 @@ package main
 
 import "testing"
 
+type test struct {
+	data   []int
+	answer int
+}
+
+func TestSomaEmTabela(t *testing.T) {
+	tests := []test{
+		test{[]int{1, 2, 3}, 6},
+		test{[]int{10, 11, 12}, 33},
+		test{[]int{-5, 0, 5, 10}, 10},
+	}
+
+	for _, v := range tests {
+		sum := soma(v.data...)
+		if sum != v.answer {
+			t.Error("Expected:", v.answer, "Got", sum)
+		}
+	}
+}
+
 func TestSoma(t *testing.T) {
 	x := soma(3, 2, 1)
 	r := 6
@@ -10,14 +30,14 @@ func TestSoma(t *testing.T) {
 	}
 }
 
-func TestSoma2(t *testing.T) {
-	x := soma(3, 2, 1)
-	r := 7
+// func TestSoma2(t *testing.T) {
+// 	x := soma(3, 2, 1)
+// 	r := 7
 
-	if x != r {
-		t.Error("Expected:", r, "Got", x)
-	}
-}
+// 	if x != r {
+// 		t.Error("Expected:", r, "Got", x)
+// 	}
+// }
 
 func TestMultplicação(t *testing.T) {
 	x := multiplica(10, 10)
